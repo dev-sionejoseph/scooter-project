a5c794b8c6abb0b011158548fc72da025c7baee9
Sione <dev.sionejoseph@gmail.com>
2024-09-13 15:00:38 -0500

    User class complete and all tests passing

--- src/User.js
+++ src/User.js
@@ -1,5 +1,26 @@
 class User {
-  // User code here
+
+  constructor(username, password, age){
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;  
+  }
+
+  login(password){
+    if (this.loggedIn){
+      throw new Error('user already logged in');
+    }
+    if (this.password === password){
+      this.loggedIn = true;
+    } else {
+      throw new Error('incorrect password')
+    }
+  }
+
+  logout(){
+    this.loggedIn = false;
+  }
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -9,10 +9,36 @@ describe('User property tests', () => {
     expect(typeof user.username).toBe('string')
   })
   // test password
-
+  test('password should be a string', () =>{
+    expect(typeof user.password).toBe('string')
+  })
   // test age
+  test('age should be a number', () =>{
+    expect(typeof user.age).toBe('number')
+  })
 })
 
 // test login
-
-// test logout
+describe("User method tests", () => {
+  test('able to log in with correct password', ()=>{
+    user.login('test123')
+    expect(user.loggedIn).toBe(true);
+  })
+  
+  test('login attempt with incorrect password throws error', () => {
+    user.logout();
+    
+    expect(()=>{
+      user.login('superwrong');
+    }).toThrow("incorrect password")
+  })
+  
+  // test logout
+  
+  test('logout functions properly', () => {
+    user.login('test123');
+    user.logout();
+  
+    expect(user.loggedIn).toBe(false);
+  })
+})
