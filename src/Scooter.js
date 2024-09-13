9184784137a322e90634c1e70918ee8563d3ff18
Sione <dev.sionejoseph@gmail.com>
2024-09-13 13:30:37 -0500

    Scooter class complete and all tests passing

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,45 @@
 class Scooter {
-  // scooter code here
+  static nextSerial = 1;
+
+  constructor(station){
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial;
+    this.charge = 100;
+    this.isBroken = false;
+
+    Scooter.nextSerial++;
+  }
+
+  rent(user){
+    if (this.charge > 20 && !this.isBroken){
+      this.station = null;
+      this.user = user;
+    } else {
+      throw new Error("Scooter needs to charge or be repaired.")
+    }
+  }
+
+  dock(station){
+    this.station = station;
+    this.user = null;
+  }
+
+  recharge(){
+
+    while (this.charge < 100){
+
+      setTimeout(()=> {    //increment charge every minute
+        this.charge++
+      }, 60000);
+
+      setTimeout(()=> {  //console log charge percentage
+        console.log(this.charge);
+      }, 120000)
+
+    }
+
+  }
 }
 
 module.exports = Scooter
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,4 +1,15 @@
-const Scooter = require('../src/Scooter')
+let Scooter;
+let User;
+
+beforeEach(() => {
+  Scooter = require('../src/Scooter');
+  User = require('../src/User');
+})
+
+afterEach(() =>{
+  jest.resetModules();
+})
+
 
 // typeof scooter === object
 describe('scooter object', () => {
@@ -8,16 +19,85 @@ describe('scooter object', () => {
   })
 })
 
+
 // Method tests
 describe('scooter methods', () => {
+
   // tests here!
+  test('correctly assigns argument', () => {
+    const station = "Shinjuku";
+    const scoot = new Scooter(station);
+
+    expect(scoot.station).toBe(station);
+  })
+
+  test('user initializes as null', () => {
+    const weeee = new Scooter("Roppongi")
+
+    expect(weeee.user).toBe(null);
+
+  })
+
+  test('serial number assigns and increments correctly', () => {
+    const speedy = new Scooter("Shibuya");
+    const wheels = new Scooter("Akihabara")
+
+    expect(speedy.serial).toEqual(1);
+    expect(wheels.serial).toEqual(2);
+  })
+
+  test('charge starts at 100', () => {
+    const pyewn = new Scooter('Ginza');
+
+    expect(pyewn.charge).toEqual(100);
+    
+  })
+
+  test('isBroken initializes as false', () => {
+    const skrt = new Scooter('Ueno');
+
+    expect(skrt.isBroken).toBe(false);
+    
+  })
 
   // rent method
 
+  test('rent method works properly with operational scooter', () => {
+    const mcqueen = new User("lightning", "I<3mater", 20);
+    const kachow = new Scooter("Radiator Springs")
+    kachow.rent(mcqueen);
+    
+    expect(kachow.user).toBe(mcqueen)
+    
+  })
+
+  test('rent method throws error with low battery/broken scooter', () => {
+    const mcqueen = new User("lightning", "I<3mater", 20);
+    const kachow = new Scooter("Radiator Springs")
+    kachow.isBroken = true;
+    
+    expect(() => {
+      kachow.rent(mcqueen);
+    }).toThrow("Scooter needs to charge or be repaired.")
+    
+  })
+
   // dock method
+
+  test('dock method works properly', () => {
+    const mcqueen = new User("lightning", "I<3mater", 20);
+    const kachow = new Scooter("Radiator Springs")
+    kachow.rent(mcqueen);
+    kachow.dock("London");
+
+    expect(kachow.user).toBe(null);
+    expect(kachow.station).toEqual("London")
+    
+  })
 
   // requestRepair method
 
   // charge method
+
 
 })
